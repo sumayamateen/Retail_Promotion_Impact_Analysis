cb25849f73c3b2b8330b23d0a1deb9c193aac8fd
Sumaya Mateen <sumayamateen6@gmail.com>
2025-09-16 15:44:56 +0500

    Add query for products during discount.sql

--- sql/08_products_during_discount.sql
+++ sql/08_products_during_discount.sql
@@ -1,19 +1,25 @@
-SELECT dm.category,
-       SUM(fe.base_price * fe.quantity_sold_after_promo) - SUM(fe.base_price * fe.quantity_sold_before_promo) AS sales_lift_revenue,
-       SUM(fe.quantity_sold_after_promo) - SUM(fe.quantity_sold_before_promo) AS sales_lift_sold_units,
-       (SUM(fe.quantity_sold_after_promo) - SUM(fe.quantity_sold_before_promo)) * 100 / SUM(fe.quantity_sold_before_promo) AS sales_lift_sold_percentage,
-       COUNT(DISTINCT fe.product_code) AS products_discounted,
-       COUNT(DISTINCT fe.campaign_id) AS campaigns_included
-	   
-FROM u131628650_supermart365.fact_events fe
+WITH discount_sales AS (
+    SELECT
+        dp.category,
 
-INNER JOIN u131628650_supermart365.dim_products dm 
-ON fe.product_code = dm.product_code
+        SUM(fe.base_price * fe.quantity_sold_after_promo) - SUM(fe.base_price * fe.quantity_sold_before_promo) AS sales_lift_revenue,
+        SUM(fe.quantity_sold_after_promo) - SUM(fe.quantity_sold_before_promo) AS sales_lift_sold_units,
+        (SUM(fe.quantity_sold_after_promo) - SUM(fe.quantity_sold_before_promo)) * 100 / SUM(fe.quantity_sold_before_promo) AS sales_lift_sold_percentage
 
-WHERE (fe.promo_type LIKE '%OFF%'
-       OR fe.promo_type LIKE '%DISC%'
-       OR fe.promo_type LIKE '%SALE%')
-	   
-GROUP BY dm.category
+    FROM fact_events fe
+    INNER JOIN dim_products dp
+    ON fe.product_code = dp.product_code
 
-ORDER BY sales_lift_sold_percentage DESC
+    WHERE fe.promo_type IN ('50% OFF', '33% OFF', '25% OFF')  
+
+    GROUP BY dp.category
+)
+SELECT
+    category,
+    sales_lift_revenue,
+    sales_lift_sold_units,
+    sales_lift_sold_percentage
+
+FROM discount_sales
+
+ORDER BY sales_lift_revenue DESC;
