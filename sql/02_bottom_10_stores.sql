b08e848056582cf860ea10aa3157cb56bc90d06c
Sumaya Mateen <sumayamateen6@gmail.com>
2025-09-16 15:27:06 +0500

    Add query for Bottom 10 performing stores

--- sql/02_bottom_10_stores.sql
+++ sql/02_bottom_10_stores.sql
@@ -1,6 +1,6 @@
 SELECT  
 	store_id,
-    SUM(quantity_sold_after_promo) - SUM(quantity_sold_before_promo) AS incremental_sold_units,
+    SUM(quantity_sold_after_promo) - SUM(quantity_sold_before_promo) AS incremental_sold_units
     
 FROM fact_events
 
